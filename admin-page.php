1e8c960205351211397d872ef504f6187ddc9fcf
Tim Brugman <tim@timbrugman.com>
2020-02-22 23:49:28 +0100

    Added setting field.

--- admin-page.php
+++ admin-page.php
@@ -11,6 +11,16 @@ function cpd_admin_page()
 
     <h2><?php _e( 'Capital P Dangit', 'capital-p-dangit' ); ?></h2>
 
+    <form method="post" action="options.php">
+<?php
+
+settings_fields('capital-p-dangit');
+do_settings_sections('capital-p-dangit');
+submit_button();
+
+?>
+    </form>
+
 </div><!-- wrap -->
 <?php
 }
@@ -20,7 +30,7 @@ function cpd_admin_page()
  */
 
 add_action( 'admin_menu', function () {
-    add_management_page(
+    add_options_page(
         'Capital P Dangit',
         'Capital P Dangit',
         'manage_options',
@@ -28,4 +38,49 @@ add_action( 'admin_menu', function () {
         'cpd_admin_page'
     );
 });
+
+/**
+ * Sections and fields: Register.
+ */
+
+add_action( 'admin_init', function () {
+    add_settings_section(
+        'cpd_section_1',
+        '',
+        false,
+        'capital-p-dangit'
+    );
+    add_settings_field(
+        'cpd_field_1',
+        'Field 1',
+        'cpd_field_1_cb',
+        'capital-p-dangit',
+        'cpd_section_1'
+    );
+    register_setting( 'capital-p-dangit', 'cpd_field_1' );
+});
+
+/**
+ * Sections: HTML.
+ */
+
+function cpd_section_1_cb( $args )
+{
+    // $args['id']
+    // $args['title']
+    // $args['callback']
+
+    echo 'string';
+}
+
+/**
+ * Fields: HTML.
+ */
+
+function cpd_field_1_cb()
+{
+?>
+<input type="text" name="cpd_field_1" id="cpd_field_1" value="<?=get_option( 'cpd_field_1' );?>">
+<?php
+}
 
